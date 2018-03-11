1e0c4b6f8e75de84c62eaf3f36c52cec1ff38422
Kevin Guran <kevin@gruzzlymug.com>
2018-03-11 00:19:25 -0600

    improve use of associations (#6)

--- web/views/album_view.ex
+++ web/views/album_view.ex
@@ -1,3 +1,18 @@
 defmodule Recollect.AlbumView do
   use Recollect.Web, :view
+  alias Recollect.Repo
+  alias Recollect.Album
+  alias Recollect.Artist
+  alias Recollect.Label
+
+  # TODO: this should work with the association (currently broken)
+  def artist_name(%Album{artist_id: artist_id}) do
+    artist = Repo.get_by!(Artist, id: artist_id)
+    artist.name
+  end
+
+  def label_name(%Album{label_id: label_id}) do
+    label = Repo.get_by!(Label, id: label_id)
+    label.name
+  end
 end
