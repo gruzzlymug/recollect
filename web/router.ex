c7a0087fef0739a5f93fffe3eff1fa6cdfd42128
Kevin Guran <kevin@gruzzlymug.com>
2018-03-12 00:06:35 -0500

    add basic CSV support (#12)
    * add basic CSV support
    
    * adjust path

--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,12 @@ defmodule Recollect.Router do
     resources "/albums", AlbumController
   end
 
+  scope "/csv", Recollect do
+    pipe_through :browser # Use the default browser stack
+
+    get "/albums", CsvController, :export_albums
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", Recollect do
   #   pipe_through :api
