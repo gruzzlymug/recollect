86c2af810b2dcfb903b4e80e6c91da46c032920b
Kevin Guran <kevin@gruzzlymug.com>
2018-03-12 02:46:24 -0500

    add query for future csv work (#16)

--- web/controllers/csv_controller.ex
+++ web/controllers/csv_controller.ex
@@ -10,6 +10,34 @@ defmodule Recollect.CsvController do
     |> send_resp(200, csv_albums())
   end
 
+  #
+  # SELECT
+  #   artist,
+  #   SUM(1) AS releases,
+  #   MIN(year) AS first,
+  #   MAX(year) AS last,
+  #   STRING_AGG(title, ' ') AS title_words
+  # FROM albums
+  # GROUP BY artist
+  # ORDER BY first, artist
+  #
+  #            artist       | releases | first | last |                    title_words
+  #      -------------------+----------+-------+------+---------------------------------------------------
+  #       Black Sabbath     |        4 |  1970 | 1975 | Black Sabbath Paranoid Master of Reality Sabotage
+  #       Rolling Stones    |        2 |  1971 | 1978 | Sticky Fingers Some Girls
+  #       King Tubby        |        1 |  1974 | 1974 | The Roots of Dub
+  #       Black Flag        |        1 |  1981 | 1981 | Damaged
+  #       Beastie Boys      |        2 |  1986 | 1989 | Licensed to Ill Paul's Boutique
+  #       PJ Harvey         |        1 |  1993 | 1993 | Rid of Me
+  #       Radiohead         |        3 |  1995 | 2000 | The Bends OK Computer Kid A
+  #       NSYNC             |        1 |  2000 | 2000 | No Strings Attached
+  #       My Morning Jacket |        1 |  2008 | 2008 | Evil Urges
+  #       The Growlers      |        3 |  2009 | 2014 | Are You In or Out? Hung at Heart Chinese Fountain
+  #       Portugal. The Man |        2 |  2010 | 2013 | American Ghetto Evil Friends
+  #       Parquet Courts    |        1 |  2012 | 2012 | Light Up Gold
+  #       Camera Obscura    |        1 |  2013 | 2013 | Desire Lines
+  #       Night Beats       |        1 |  2016 | 2016 | Who Sold My Generation
+  #
   defp csv_albums do
     Album
     |> Repo.all
